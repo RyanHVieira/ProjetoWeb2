8cd9b9eaf9a5a214170ba91983d9122ec6d4e10d
Ryan <ryanhevieira@gmail.com>
2026-08-06 15:06:44 -0300

    env db e afins

--- backend/Models/Product.cs
+++ backend/Models/Product.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Backend.Models{
+    [Table("Products")]
     public class Product{
+        [Column("id")]
         public int Id { get; set; }
+        [Column("name")]
         public string Name { get; set; }
+        [Column("price")]
         public decimal Price { get; set; }
+        [Column("description")]
         public string Description { get; set; }
+        [Column("image_url")]
         public string ImageUrl { get; set; }
     }
 }
