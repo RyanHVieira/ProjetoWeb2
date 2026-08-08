26d5ef78429e14eb0d376a3a85e3239aee6a5099
Ryan <ryanhevieira@gmail.com>
2026-08-08 02:03:24 -0300

    swagger falta bearer token

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,82 +6,47 @@ using backend.services.authservice;
 using backend.services.products;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-
 Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = Environment.GetEnvironmentVariable(
-    "ConnectionStrings__DefaultConnection"
-);
-
-
+var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
 var jwtKey = Environment.GetEnvironmentVariable("Jwt__Key");
 var jwtIssuer = Environment.GetEnvironmentVariable("Jwt__Issuer");
 var jwtAudience = Environment.GetEnvironmentVariable("Jwt__Audience");
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(connectionString)
-);
+builder.Services.AddDbContext<AppDbContext>(options =>options.UseNpgsql(connectionString));
 
-
-builder.Services
-    .AddAuthentication("Bearer")
-    .AddJwtBearer("Bearer", options =>
-    {
-        options.TokenValidationParameters = new()
-        {
+builder.Services.AddAuthentication("Bearer").AddJwtBearer("Bearer", options =>{
+        options.TokenValidationParameters = new(){
             ValidateIssuer = true,
             ValidateAudience = true,
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
-
             ValidIssuer = jwtIssuer,
             ValidAudience = jwtAudience,
-
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(jwtKey!)
-            )
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey!))
         };
-    });
+    }
+);
 
 builder.Services.AddAuthorization();
-
-
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("ReactPolicy", policy =>
-    {
-        policy
-            .AllowAnyOrigin()
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-    });
-});
-
+builder.Services.AddCors(options =>{options.AddPolicy("ReactPolicy", policy =>{policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();});});
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped<JwtService>();
-
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseHttpsRedirection();
-
 app.UseCors("ReactPolicy");
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
